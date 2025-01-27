706977e8d572a82fb5317fd296c8cce47ae19695
Vova_Solovey <millsmillsov@yandex.ru>
2025-01-27 19:02:09 +0200

    добавлены функции преобразования csv в sql и sql в csv, так же считывание из sql строк таблицы

--- main.py
+++ main.py
@@ -4,3 +4,29 @@ import pandas as pd
 conn = sqlite3.connect('database.db')
 cursor = conn.cursor()
 
+# функция считывания CSV в SQl
+
+def csv2sql(filePath, tableName):
+	df = pd.read_csv(filePath)
+	df.to_sql(tableName, con=conn, if_exists='replace', index=False)
+
+
+# функция преобразования из sql в csv
+def sql2csv(tableName, filePath):
+	df = pd.read_sql(sql=f'SELECT * FROM {tableName}', con=conn)
+	df.to_csv(filePath, con=conn, index=False)
+
+# функция отображения таблицы
+
+def showTable(tableName):
+	cursor.execute(f'SELECT * FROM {tableName}')
+	
+	for row in cursor.fetchall():
+		print(row)
+
+
+# csv2sql('store/data.csv', 'tmp_auto')
+
+# showTable('tmp_auto')
+
+sql2csv('tmp_auto', 'test.csv')
